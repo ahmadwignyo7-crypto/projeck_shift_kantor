d0a349ab4808a20c7f750510dcaf3311c1650ddd
ahmadwignyo7-crypto <ahmadwignyo7@gmail.com>
2026-09-06 17:11:34 +0800

    feat:api 2

--- api/index.py
+++ api/index.py
@@ -1,8 +1,12 @@
 import sys
 import os
+import traceback
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from main import create_app
-
-app = create_app()
+try:
+    from main import create_app
+    app = create_app()
+except Exception as e:
+    traceback.print_exc()
+    raise e
